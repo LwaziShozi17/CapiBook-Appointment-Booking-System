7369959cf8657160699bf75a8e9fb80e81c3cd9a
LwaziShozi17 <LwaziShozi+capitec@capitecbank.co.za>
2026-08-28 08:43:10 +0200

    Initial commit

---
+++ src/test/java/com/capitec/CapiBook/CapiBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capitec.CapiBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapiBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
